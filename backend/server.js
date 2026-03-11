d5781cf1f8508e82bdb65b4bba16db6d7677b75e
ruin <mjakob478@gmail.com>
2026-03-10 22:46:47 -0500

    useFollow hook and suggested users fetching

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Middleware
-app.use(express.json());
+app.use(express.json({ limit: "5mb" })); //Default limit is 100kb
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
